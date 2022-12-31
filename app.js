3a08f0971b7cba57e8d607ef30903cb8dc8d2c36
HydraRetro <119288194+HydraRetro@users.noreply.github.com>
2022-12-31 00:42:54 -0800

    Finished part 1 of the project

--- app.js
+++ app.js
@@ -1,64 +1,68 @@
 //Rock Paper Scissors
+let playerScore = 0
+let computerScore = 0
 
-
-//Ask player for choice
-let playerPrompt = prompt("Rock, Paper, or Scissors?")
-
-//Take players answer, convert to all lowercase
-let playerSelection = playerPrompt.toLowerCase();
-
-
-//Computer Random Choice
-const computerSelection = getComputerChoice();
-
-function getComputerChoice() {
-    let rand = Math.floor(Math.random() * 3);
-    if (rand === 0) {
-        return ("rock");
-    } else if (rand === 1) {
-        return ("paper");
-    } else {
-        return ("scissors");
+//LOOP THE GAME FUNCTION 5 TIMES
+for (let i = 0; i < 5; i++) {
+    //ASK PLAYERS CHOICE
+    let playerPrompt = prompt("Rock, Paper, or Scissors?");
+    //LOWERCASE USER INPUT
+    let playerSelection = playerPrompt.toLowerCase();
+    //GET RANDOM COMPUTER CHOICE
+    function getComputerChoice() {
+        let rand = Math.floor(Math.random() * 3);
+        if (rand === 0) {
+            return ("rock");
+        } else if (rand === 1) {
+            return ("paper");
+        } else {
+            return ("scissors");
+        }
     }
-}
-
-
-//Function to play a single round
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock") {
-        if (computerSelection === "rock") {
+    //STORE COMPUTER CHOICE
+    const computerSelection = getComputerChoice();
+    //CREATE FUNCTION TO PLAY A ROUND
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
             return ("TIE GAME");
-        } else if (computerSelection === "paper") {
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            computerScore++;
             return ("Paper covers Rock, You Lose!");
-        } else if (computerSelection === "scissors") {
-            return ("Rock beats Scissors, You Win!");
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore++;
+            return ("Rock beats Scissors. You Win!");
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerScore++;
+            return ("Paper covers Rock. You Win!");
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            computerScore++;
+            return ("Scissors cuts Paper. You Lose!");
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            computerScore++;
+            return ("Rock beats Scissors. You Lose!");
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerScore, computerScore
+            playerScore++
+            return ("Scissors cuts Paper. You Win!");
+        } else {
+            return ("Try again, yo.");
         }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "paper") {
-            return ("TIE GAME");
-        } else if (computerSelection === "rock") {
-            return ("Paper covers Rock, You Win");
-        } else if (computerSelection === "scissors") {
-            return ("Scissors cuts Paper, You Lose!");
-        }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "scissors") {
-            return ("TIE GAME");
-        } else if (computerSelection === "rock") {
-            return ("Rock beats Scissors, You Lose!");
-        } else if (computerSelection === "paper") {
-            return ("Scissors cuts Paper, You Win!");
-        }
+    }
+    //CONSOLE.LOG THE RESULTS
+    console.log("Computer picks: ", computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("Players Score is ", playerScore);
+    console.log("Computer Score is ", computerScore);
+}
+//CHECK IF GAME IS OVER
+function isGameOver() {
+    if (playerScore === computerScore) {
+        return ("TIE GAME. PLAY AGAIN");
+    } else if (playerScore > computerScore) {
+        return ("YOU WIN");
     } else {
-        return ("Error! try again, yo.");
+        return ("You lose sucka!")
     }
 }
-// console.log(playerSelection);
-console.log("Computer picks: ", computerSelection);
 
-console.log(playRound(playerSelection, computerSelection));
-
-//Put the playRound function in a loop to play 5 rounds
-
-
-//keep the score and report winner at end.
+console.log(isGameOver())
