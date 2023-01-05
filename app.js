1b17fe3f2ac73bf53dfccd8232836e664e8755ea
HydraRetro <119288194+HydraRetro@users.noreply.github.com>
2023-01-04 17:37:22 -0800

    Reworked event listener function

--- app.js
+++ app.js
@@ -1,88 +1,83 @@
 //Rock Paper Scissors
-const playerChoice = document.getElementById('player-choice');
-const computerChoice = document.getElementById('computer-choice');
-const playerScoreDisplay = document.getElementById('player-score');
-const computerScoreDisplay = document.getElementById('computer-score');
-const resultDisplay = document.getElementById('result');
 
-//Player Score & Display
-let playerScore = 0
-playerScoreDisplay.textContent = playerScore;
-//Computer Score & Display
-let computerScore = 0
-computerScoreDisplay.textContent = computerScore;
+//Player Score
+let playerScore = 0;
+//Computer Score
+let computerScore = 0;
 //Results
-let result = 0;
+let result;
 //Max Score 
-let maxScore = 5;
+let winningScore = 6;
+
+//Player Selection
+let computerSelection;
+let playerSelection;
 
 //Event listener for all buttons
 const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        // playerChoice.textContent = button.id;//print player choice on display
-        // console.log(button.id);//TEMP console log
+buttons.forEach(button => button.addEventListener('click', () => {
 
-        function game() {
-            let playerSelection = button.id;
-            playerChoice.textContent = playerSelection;
-            let computerSelection = getComputerChoice();
-            computerChoice.textContent = computerSelection;
-            return playRound(playerSelection, computerSelection);
+    playerSelection = button.id;//Gets player value from button id
+    computerSelection = getComputerChoice();//Gets computer value from function
+    playRound(playerSelection, computerSelection);//Runs the game function once
+    playerChoice.textContent = playerSelection;//Displays player choice
+    computerChoice.textContent = computerSelection;//Displays computer choice
+    resultDisplay.textContent = playRound(playerSelection, computerSelection);//Displays the result of one game
+    updateScores(playerScore, computerScore);//Displays updated scores
 
-            //GET RANDOM COMPUTER CHOICE
-            function getComputerChoice() {
-                let rand = Math.floor(Math.random() * 3);
-                if (rand === 0) {
-                    return ("rock");
-                } else if (rand === 1) {
-                    return ("paper");
-                } else {
-                    return ("scissors");
-                }
-            }
+    // CONSOLE.LOG THE RESULTS - TEMP
+    console.log("Player Picks: ", playerSelection);
+    console.log("Computer picks: ", computerSelection);
+    console.log("Players Score is ", playerScore);
+    console.log("Computer Score is ", computerScore);
+    console.log(resultDisplay.innerText);
 
-            //CREATE FUNCTION TO PLAY A ROUND
-            function playRound(playerSelection, computerSelection) {
-                if (playerSelection === computerSelection) {
-                    return ("TIE GAME");
-                } else if (playerSelection === "rock" && computerSelection === "paper") {
-                    computerScore++;
-                    return ("Paper covers Rock, You Lose!");
-                } else if (playerSelection === "rock" && computerSelection === "scissors") {
-                    playerScore++;
-                    return ("Rock beats Scissors. You Win!");
-                } else if (playerSelection === "paper" && computerSelection === "rock") {
-                    playerScore++;
-                    return ("Paper covers Rock. You Win!");
-                } else if (playerSelection === "paper" && computerSelection === "scissors") {
-                    computerScore++;
-                    return ("Scissors cuts Paper. You Lose!");
-                } else if (playerSelection === "scissors" && computerSelection === "rock") {
-                    computerScore++;
-                    return ("Rock beats Scissors. You Lose!");
-                } else if (playerSelection === "scissors" && computerSelection === "paper") {
-                    playerScore, computerScore
-                    playerScore++
-                    return ("Scissors cuts Paper. You Win!");
-                } else {
-                    return ("Try again, yo.");
-                }
-            }
-        }
-    })
-})
+}))
 
+//GET RANDOM COMPUTER CHOICE
+function getComputerChoice() {
+    let rand = Math.floor(Math.random() * 3);
+    if (rand === 0) {
+        return ("rock");
+    } else if (rand === 1) {
+        return ("paper");
+    } else {
+        return ("scissors");
+    }
+}
 
+//CREATE FUNCTION TO PLAY A ROUND
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return ("It's a Draw.");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore += 1;
+        return ("Paper covers Rock, You Lose!");
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore += 1;
+        return ("Rock beats Scissors. You Win!");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore += 1;
+        return ("Paper covers Rock. You Win!");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore += 1;
+        return ("Scissors cuts Paper. You Lose!");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore += 1;
+        return ("Rock beats Scissors. You Lose!");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore += 1;
+        return ("Scissors cuts Paper. You Win!");
+    } else {
+        return ("Try again, yo.");
+    }
+}
 
-
-
-
-//CONSOLE.LOG THE RESULTS - TEMP
-console.log("Computer picks: ", computerSelection);
-console.log(playRound(playerSelection, computerSelection));
-console.log("Players Score is ", playerScore);
-console.log("Computer Score is ", computerScore);
+//UPDATE SCORES
+function updateScores(playerScore, computerScore) {
+    playerScoreDisplay.innerHTML = playerScore;
+    computerScoreDisplay.innerHTML = computerScore;
+}
 
 //CHECK IF GAME IS OVER
 function isGameOver() {
@@ -94,12 +89,19 @@ function isGameOver() {
         return ("You lose sucka!")
     }
 }
-console.log(isGameOver())
+
+
+
+//UI
+const playerChoice = document.getElementById('player-choice');
+const computerChoice = document.getElementById('computer-choice');
+const playerScoreDisplay = document.getElementById('player-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const resultDisplay = document.getElementById('result');
 
 //RESET BUTTON
 const resetButton = document.querySelector('#resetButton');
 resetButton.addEventListener('click', reset)
-//TRY TO RESET WITHOUT RELOADING WHOLE PAGE.
 function reset() {
-    location.reload();
+    location.reload(); //WILL TRY TO RESET WITHOUT RELOADING WHOLE PAGE.
 }
